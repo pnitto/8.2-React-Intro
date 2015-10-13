63abd5a26906ad012088348b4c9a36a7739c2b4a
Paul Nitto <pnitto18@gmail.com>
2015-10-13 16:05:27 -0400

    exercise-2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-var App = React.createClass({
+/*var App = React.createClass({
   render(){
   return (
 <div>
@@ -19,3 +19,46 @@ var App = React.createClass({
 )}
 
 ReactDOM.render(<App />, document.getElementById('react-app'))
+
+*/
+var contacts = [
+    {key: 1, name: "James K Nelson", email: "james@jamesknelson.com", description: "Front-end Unicorn"},
+    {key: 2, name: "Jim", email: "jim@example.com"},
+    {key: 3, name: "Joe"},
+];
+
+var ContactItem = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    email: React.PropTypes.string.isRequired,
+    description: React.PropTypes.string
+  },
+  render(){
+    return (
+      <li>
+        <h2>{this.props.name}</h2>
+        <a href={'mailto:' + this.props.email}>{this.props.email}</a>
+        <p>{this.props.description}</p>
+      </li>
+    )
+  }
+});
+
+var listElements = contacts
+  .filter(function(contact) {
+    return contact.email;
+  })
+  .map(function(contact) {
+    return <ContactItem {...contact} />;
+  });
+
+var rootElement = (
+  <div>
+    <h1>Contacts</h1>
+    <ul>
+      {listElements}
+    </ul>
+  </div>
+);
+
+ReactDOM.render(rootElement, document.getElementById('react-app'))
