e2944d558c7d40fcf9e131f8bf01f4e73320843e
Paul Nitto <pnitto18@gmail.com>
2015-10-13 14:57:32 -0400

    Exercise 1

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+var App = React.createClass({
+  render(){
+  return (
+<div>
+  <h1>Contacts</h1>
+  <ul>
+    <li>
+      <h2>James Nelson</h2>
+      <a href="mailto:james@jamesknelson.com">james@jamesknelson</a>
+    </li>
+    <li>
+      <h2>Joe Citizen</h2>
+      <a href="mailto:joe@example.com">joe@example.com</a>
+    </li>
+  </ul>
+</div>
+);
+}
+)}
+
+ReactDOM.render(<App />, document.getElementById('react-app'))
